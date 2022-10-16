655f02f3e8ec0dd6d1cd24c09343463e9758d528
Chuck Roth <chuckroth@chucks-mbp.myfiosgateway.com>
2022-10-16 03:21:34 -0400

    changed images into cards

--- api/backend.js
+++ api/backend.js
@@ -6,43 +6,15 @@ const app = express()
 const zdk = new ZDK("https://api.zora.co/graphql");
 app.use(express.json())
 
-/*using built-function fetchTokens 
-which uses graphql and collectionaddresses to query a collectionAddresses token info
-*/
-async function fetchTokens(zdk, collectionAddresses){
-    return await zdk.tokens({
-      where: {
-        collectionAddresses
-      }
-    })
-  } 
-  
-/**
- * I am using modules to utilize top level await
-*/
-function myTokenData(name, description, owner,url){
+
+async function fetchTokens(zdk, ownerAddresses){
+    return await zdk.tokens({where: {ownerAddresses}})
+} 
+
+function myTokenData(name, owner,url){
     this.name = name
-    this.description = description
-    this.owner = owner
     this.url = url
 }
-/**
- * 
-*/
-function formatJSON(theJSON){
-    let x
-    let outPut = []
-    for(x=0; x< theJSON.length; x++){
-        let thisTokenData = new myTokenData(theJSON[x].token.name, 
-            theJSON[x].token.description, 
-            theJSON[x].token.owner, 
-            theJSON[x].token.image.mediaEncoding.thumbnail)
-            outPut.push(thisTokenData)
-    }
-    let astr= JSON.stringify(outPut)
-    return astr
-}
-
 function formatInput(a){
     let resArr= Object.values(a)
     let targetStr = ""
@@ -50,19 +22,23 @@ function formatInput(a){
     for(x = 0; x< resArr.length; x++){
         targetStr = targetStr + resArr[x]
     }
+    console.log(targetStr)
     return targetStr
 }
 
-
-app.get('/', (req, res)=>{
-    res.end('you are seeing information on the screen congratulations')
-})
-/**
- * hitting this directly from express is just a spewed out JSON string of the info we will pass into react
- */
-
-
-app.use('/api/nft', async (req, res, next) => {
+function formatJSON(theJSON){
+    let x
+    let outPut = []
+    for(x=0; x< theJSON.length; x++){
+        let thisTokenData = new myTokenData(theJSON[x].token.name, 
+            theJSON[x].token.name, 
+            theJSON[x].token.content.url) 
+            outPut.push(thisTokenData)
+    }
+    let astr= JSON.stringify(outPut)
+    return astr
+}
+app.use('/api/nft', async (req, res, next)=>{
     try {
         let { e } = req.body
         const inp = { e }
@@ -71,14 +47,14 @@ app.use('/api/nft', async (req, res, next) => {
         const qryString = JSON.stringify(qry, null, 3)
         const theNodes = JSON.parse(qryString).tokens.nodes
         const ourSend = formatJSON(theNodes) 
-        console.log(ourSend)
-        res.end(ourSend)
-        } catch (error) {
-        console.error("invalid query")
+        res.send(ourSend)
+    } catch (error) {
+        console.error(error)
         res.send("this is bad")
-        }
-
-
+    }
+    
 })
 
-app.listen(port, console.log(`server is listening on port ${port}`))
+app.listen(port, ()=>{
+    console.log(`Currently listening on ${port}`)
+})
--- client/src/Form.js
+++ client/src/Form.js
@@ -3,71 +3,67 @@ import axios from 'axios'
 import Card from 'react-bootstrap/Card';
 
 export default function Form() {
-    const [name, getNFT] = useState("");
-    const [pics, setPics] = useState([]);
-  
-    return (
-      <div className="App">
-        <h1>Zora NFT Query</h1>
-        <form onSubmit={async (e)=>{
-            e.preventDefault()
-            let imageArray = []
-            const resp = await postName(name)
-            if (resp === "Internal Server Error"){
-                imageArray = "Bad Query"
-            } else {
-            let x
-            for(x=0; x<resp.length; x++){
-                
-                if(!resp[x].url){
-                    continue
-                } else{
-                    const nftName = resp[x].name
-                    const nftDescription = resp[x].description
-                    const nftImage = resp[x].url
-                    const nftOWner = resp[x].owner
-                    //imageArray.push(<div><img src={nftImage} alt={nftDescription} loading="lazy"/><h1>{nftName}</h1><p>owner: {nftOWner}</p></div>)
-                    imageArray.push(
-                        <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={nftImage} alt= {nftDescription}/>
-                        <Card.Body>
-                        <Card.Title>{nftName}</Card.Title>
-                        <Card.Text>
-                        owner: {nftOWner}
-                        </Card.Text>
-                        </Card.Body>
-                        </Card>
-                    )
-                }
-                
-                
-            }
-            }
-            setPics(imageArray)
-        }}>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => getNFT(e.target.value)}
-          />
-          <button type="submit">Enter Collection Address</button>
-        </form>
-        {pics}
-      </div>
-    );
+  const [name, getNFT] = useState("");
+  const [pics, setPics] = useState([]);
+
+  return (
+    <div className="App">
+      <form onSubmit={async (e)=>{
+          e.preventDefault()
+          let imageArray = []
+          const resp = await postName(name)
+          if (resp === "Internal Server Error"){
+              imageArray = "Bad Query"
+          } else {
+          let x
+          for(x=0; x<resp.length; x++){
+              
+              if(!resp[x].url){
+                  continue
+              } else{
+                  const nftName = resp[x].name
+                  const nftImage = resp[x].url
+                  imageArray.push
+                  (<Card style={{ width: '30rem', display: 'grid'}}>
+                      <Card.Img variant="top" src={nftImage} alt= "Internal Server Error"style={{ width: '20rem' }} />
+                      <Card.Body>
+                      <Card.Title>Piece Name: {nftName}</Card.Title>
+                      <Card.Text>
+                      </Card.Text>
+                      </Card.Body>
+                      </Card>)
+              }
+              
+              
+          }
+          }
+          setPics(imageArray)
+      }}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => getNFT(e.target.value)}
+        />
+        <button type="submit">Enter Owner Address</button>
+      </form>
+      <div style={{display: 'grid', 'grid-template-columns': 'repeat(5, 1fr)',' grid-row-gap': '1rem'}}>{pics}</div>
+      
+    </div>
+  );
 }
 
 async function postName (e) {
-      try {
-        const resp = await axios.post('/api/nft',{ e })
-        console.log(resp.data)
-        
-        if (resp.data === "this is bad"){
-            return "Internal Server Error"
-        }
-        return resp.data
-      } catch (error) {
-        console.error(error);
-        return "Internal Server Error"
+    try {
+      const resp = await axios.post('/api/nft',{ e })
+      console.log(resp.data)
+      
+      if (resp.data === "this is bad"){
+          return "Internal Server Error"
       }
+      return resp.data
+    } catch (error) {
+      console.error(error);
+      return "Internal Server Error"
+    }
 }
+
