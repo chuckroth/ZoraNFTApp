38b576bb2d9c09a23ea393d2868382c0096b902b
Chuck Roth <chuckroth@Chucks-MBP.lan>
2022-10-17 20:58:17 -0400

    added loading functionality

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,18 +1,53 @@
 import React, {useState} from "react";
 import axios from 'axios'
 import Card from 'react-bootstrap/Card';
+//import * as ReactBootstrap from "react-bootstrap"
 
 //probably would have been better suited as a class but useState felt more elegant
 export default function Form() {
   const [name, getNFT] = useState(""); 
   const [pics, setPics] = useState([]);
+  //below is a test
+  const [loading, setLoading] = useState(false)
+  async function getGal (e) {
+    try {
+      const resp = await axios
+        .post('/api/nft',{ e })
+        .then(res =>{
+          setLoading(true)
+          console.log("this is fine")
+          if(res.data ==="this is bad"){
+            return "Internal Server Error"
+          }
+          return res.data
+          
+        })
+      /*  
+      THIS IS THE ORIGINAL CODE
+      if (resp.data === "this is bad"){
+      return "Internal Server Error"
+      }
+        return resp.data*/
+      //console.log(resp.data)
+      
+      //this is what I set the return value to in express route's try catch
+      return resp
+    } catch (error) {
+      console.error(error);
+      return "Internal Server Error"
+    }
+}
 
+
+  
   return (
     <div className="App">
       <form onSubmit={async (e)=>{
           e.preventDefault()
+          setLoading(true)
           let imageArray = []
           const resp = await getGal(name)
+          setLoading(false)
           if (resp === "Internal Server Error"){
             //invalid query displays text instead of images
               imageArray = "Bad Query"
@@ -51,25 +86,10 @@ export default function Form() {
         />
         <button type="submit">Enter Owner Address</button>
       </form>
+      {loading ? ("i am loading") : ("")}
       <div style={{display: 'grid', 'grid-template-columns': 'repeat(5, 1fr)',' grid-row-gap': '1rem'}}>{pics}</div>
       
     </div>
   );
-}
-
-async function getGal (e) {
-    try {
-      const resp = await axios.post('/api/nft',{ e })
-      console.log(resp.data)
-      
-      //this is what I set the return value to in express route's try catch
-      if (resp.data === "this is bad"){
-          return "Internal Server Error"
-      }
-      return resp.data
-    } catch (error) {
-      console.error(error);
-      return "Internal Server Error"
-    }
 }
 
